e474ced20dd7c97608dc0c8c6d9cad8cce4267c9
CaioRD <caiords67@gmail.com>
2025-02-26 21:14:28 -0300

    add final commit

--- Formação angular developer/Trabalhando com web component/challenge-dio-html01/src/components/cardNews.js
+++ Formação angular developer/Trabalhando com web component/challenge-dio-html01/src/components/cardNews.js
@@ -1,54 +1,85 @@
-class CardNews extends HTMLElement{
-    constructor(){
-        super();
+class CardNews extends HTMLElement {
+  constructor() {
+    super();
 
-        const shadow = this.attachShadow({mode:"open"});
-        shadow.appendChild(this.build())
-        shadow.appendChild(this.styles())
-    }
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
+  }
 
-    build(){
-        const componentRoot = document.createElement("div");
-        componentRoot.setAttribute("class", "card")
-        
-        const cardLeft = document.createElement("div");
-        cardLeft.setAttribute("class", "card-left");     
+  build() {
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "card");
 
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card-left");
 
-        const spanText = document.createElement("span"); 
-        spanText.textContent = "by " + this.getAttribute("spanText") || "Anonymous";
-        
-        const tittle = document.createElement("h2");
-        tittle.textContent = this.getAttribute("tittle") || "No Title";
+    const spanText = document.createElement("span");
+    spanText.textContent = "by " + this.getAttribute("spanText") || "Anonymous";
 
-        const paragraph = document.createElement("p");
-        paragraph.textContent = this.getAttribute("paragraph")        
+    const tittle = document.createElement("h2");
+    tittle.textContent = this.getAttribute("tittle") || "No Title";
 
-        cardLeft.appendChild(spanText);
-        cardLeft.appendChild(tittle);
-        cardLeft.appendChild(paragraph);
+    const paragraph = document.createElement("p");
+    paragraph.textContent = this.getAttribute("paragraph");
 
+    cardLeft.appendChild(spanText);
+    cardLeft.appendChild(tittle);
+    cardLeft.appendChild(paragraph);
 
-        const cardRigth = document.createElement("div");
-        cardRigth.setAttribute("class", "card-right");
-        
-        const image = document.createElement("img");
-        image.src = this.getAttribute("image") || "assets/foto-default.png";
-        image.alt = "imagem da notícia";
+    const cardRigth = document.createElement("div");
+    cardRigth.setAttribute("class", "card-right");
 
-        cardRigth.appendChild(image);
+    const image = document.createElement("img");
+    image.src = this.getAttribute("image") || "assets/foto-default.png";
+    image.alt = "imagem da notícia";
 
+    cardRigth.appendChild(image);
 
-        componentRoot.appendChild(cardLeft);
-        componentRoot.appendChild(cardRigth);
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRigth);
 
     return componentRoot;
-    }
+  }
 
+  styles() {
+    const style = document.createElement("style");
 
-    styles(){
+    style.textContent = `
+        .card{
+    width: 80%;
+    -webkit-box-shadow: -1px 1px 27px 3px rgba(0,0,0,0.68);
+    -moz-box-shadow: -1px 1px 27px 3px rgba(0,0,0,0.68);
+    box-shadow: -1px 1px 27px 3px rgba(0,0,0,0.68);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+.card-left{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 18px;
+}
+.card-left span{
+    font-weight: 600;
+}
+.card-left h2{
+    margin-top: 16px;
+    font-size: 22px;
+}
+.card-left p {
+  color: gray;
+}
 
-    }
+img{
+    width: 350px;
+}
+        `;
+
+    return style;
+  }
 }
 
 customElements.define("card-news", CardNews);
